13fbdeaccd634cbeed301f570414a6b087b2b444
Kevsi <kevsertuerkmen@gmail.com>
2026-05-13 15:12:48 +0200

    Move website files to root for GitHub Pages

--- feed.js
+++ feed.js
@@ -1,7 +1,9 @@
 import { auth } from "./firebase-config.js";
-import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
+import { onAuthStateChanged } from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
 
 onAuthStateChanged(auth, (user) => {
+    console.log("Feed user:", user);
+
     if (!user) {
         window.location.href = "login.html";
     }
